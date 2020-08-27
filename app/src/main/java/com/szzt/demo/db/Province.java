ac10489570d22ea83c545fb4f3a9fee1a75ac2be
gaoyang <1034488794@qq.com>
2020-08-27 11:23:42 +0800

    加入数据库和表的各项配置

---
+++ app/src/main/java/com/szzt/demo/db/Province.java
@@ -0,0 +1,33 @@
+package com.szzt.demo.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private   int  id;
+    private   String  provinceNmae;//记录省的名字
+    private   int  provinceCode;//记录省的代号
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceNmae() {
+        return provinceNmae;
+    }
+
+    public void setProvinceNmae(String provinceNmae) {
+        this.provinceNmae = provinceNmae;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
